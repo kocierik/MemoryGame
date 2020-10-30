83d97ee37f9583fe5cb765250d1f1ca533bd3e72
Erik Koci <kocierik@gmail.com>
2020-10-31 00:28:36 +0100

    feat: optimized code, fix list bug

--- app/src/main/java/com/example/memory/MainActivity.kt
+++ app/src/main/java/com/example/memory/MainActivity.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         val button: Button = findViewById(R.id.btnStart)
         button.setOnClickListener { startGame() }
         val txtGame: TextView = findViewById(R.id.txtNumber)
-        val TextNumber: EditText  = findViewById(R.id.userNumber)
+        val TextNumber: EditText = findViewById(R.id.userNumber)
         val score: TextView = findViewById(R.id.txtScore)
         txtScore.visibility = View.INVISIBLE
         val image: ImageView = findViewById(R.id.image)
@@ -29,89 +31,107 @@ class MainActivity : AppCompatActivity() {
             R.anim.rotate_clockwise
         )
         image.startAnimation(clkRotate)
-
     }
+    var timer: CountDownTimer? = null
+    var score = 0
+    var totalTime = 1000L
+    var game = true
+    var gameNumberList = mutableListOf<String>()
     @SuppressLint("SetTextI18n")
     private fun startGame(){
-        Toast.makeText(this, "The game is start!", Toast.LENGTH_SHORT).show()
-        var gameNumberList = mutableListOf<String>()
-        var lengthGameNumberList = mutableListOf<Int>()
-        var userNumberList = mutableListOf<String>()
-        var totalTime = 6000L
         score = 0
-        remove = ""
-        i = 0
-        k=0
-        txtScore.text = "score\n$score"
-        userNumber.text= null
-
+        txtScore.text = "score\n $score"
         btnStart.visibility = View.INVISIBLE
+        Toast.makeText(this, "The game is start!", Toast.LENGTH_SHORT).show()
+        getRandomNumber()
+    }
+    
+    private fun onFinishTimer(gameNumberList: MutableList<String>) {
+        game = true
+        txtNumber.text = ""
+        btnStart.visibility = View.INVISIBLE
+        userNumber.visibility = View.VISIBLE
+        txtScore.visibility = View.VISIBLE
 
-        object : CountDownTimer(totalTime, 1000) {
-
-            override fun onTick(millisUntilFinished: Long) {
-                addGameNumber(lengthGameNumberList,gameNumberList)
+        userNumber.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(
+                s: CharSequence,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
             }
 
             @SuppressLint("SetTextI18n")
+            override fun onTextChanged(
+                s: CharSequence,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                txtScore.text = "score\n$score"
+                Log.d("CHECK", "LISTA $gameNumberList")
+                checkNumber()
+            }
+        })
+    }
+
+    private fun checkNumber() {
+        Log.d("PROVA", "LISTA $gameNumberList")
+        if (userNumber.text.length == gameNumberList.max()?.length && gameNumberList.isNotEmpty()) {
+            for ((i, x) in gameNumberList.withIndex()) {
+                game = false
+                if (userNumber.text.toString() == gameNumberList[i] && userNumber.text.toString().length == gameNumberList[i].length) {
+                    game = true
+                    score++
+                    gameNumberList.removeAt(i)
+                    checkEmptyList()
+                    userNumber.text = null
+                    break
+                }
+            }
+        }
+        checkLose()
+    }
+    private fun getRandomNumber(){
+        userNumber.visibility = View.INVISIBLE
+        timer = object: CountDownTimer(totalTime, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                txtNumber.text = (10..100).random().toString()
+                gameNumberList.add(txtNumber.text as String)
+            }
             override fun onFinish() {
-
-                txtNumber.text =""
+                onFinishTimer(gameNumberList)
                 userNumber.visibility = View.VISIBLE
-                txtScore.visibility = View.VISIBLE
-                btnStart.visibility = View.INVISIBLE
-                var number = ""
-
-                userNumber.addTextChangedListener(object : TextWatcher {
-                    override fun afterTextChanged(s: Editable) {}
-                    override fun beforeTextChanged(s: CharSequence,start: Int, count: Int, after: Int) {
-                    }
-                    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int){
-                        checkNumber(gameNumberList, userNumberList)
-                        txtScore.text = "score\n$score"
-                        }
-                })
             }
         }.start()
     }
 
-    private fun addGameNumber(lengthGameNumberList: MutableList<Int>, gameNumberList: MutableList<String>){
-        txtNumber.text =  (10..100).random().toString()
-        lengthGameNumberList.add(txtNumber.text.length)
-        gameNumberList.add(txtNumber.text as String)
-    }
-
-    var score = 0
-    var i = 0
-    var bool = false
-    var k = 0
-    var remove = ""
-    @SuppressLint("SetTextI18n", "ResourceType")
-    private fun checkNumber(gameNumberList: MutableList<String>, userNumberList: MutableList<String>) {
-        if (userNumber.text.length == gameNumberList.max()?.length) {
-            userNumberList.add(userNumber.text.toString())
-            for (x in gameNumberList) {
-                if (userNumberList[i] == gameNumberList[k] && userNumberList[i].length == gameNumberList[k].length) {
-                    score += 1
-                    bool = true
-                }
-                k +=1
-            }
-
-            i += 1
-            k = 0
-
-            userNumber.text = null
-            if(!bool){
-                txtNumber.text = "You Lose"
-                userNumber.visibility = View.INVISIBLE
-                btnStart.visibility = View.VISIBLE
-                i=0
-                gameNumberList.clear()
-                userNumberList.clear()
-            }
-            bool = false
+    private fun checkEmptyList(){
+        if(gameNumberList.isEmpty()) {
+            timer?.cancel()
+            timer = null
+            totalTime += 1000
+            getRandomNumber()
         }
+    }
+    private fun checkLose(){
+        if(!game) {
+            Log.d("HAI PERSO", "ok")
+            txtNumber.text = getString(R.string.txt_lose)
+            userNumber.visibility = View.INVISIBLE
+            btnStart.visibility = View.VISIBLE
+            gameReset()
+        }
+    }
+    private fun gameReset(){
+        totalTime = 1000L
+        game = true
+        timer?.cancel()
+        timer = null
+        gameNumberList.clear()
+        userNumber.text = null
     }
 }
 
